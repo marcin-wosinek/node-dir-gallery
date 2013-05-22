9f277efd1af18813cdd0f89111ad2194b1fc2c2a
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-05-22 14:15:18 +0200

    Display all images form folder

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   var images = fs.readdirSync('images');
-  console.log(images);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  var output = '';
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  for (var i in images) {
+    output += '<img src="/images/' + images[i] + '" /><br />';
+  }
+
+  res.end(output);
 }).listen(1337);
