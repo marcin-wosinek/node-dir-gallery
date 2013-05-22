47fc5e1d891c58be9e49d21669a502303e8cfb9f
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-05-22 14:08:22 +0200

    List images

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var http = require('http');
+var fs = require('fs');
+
 http.createServer(function (req, res) {
+  var images = fs.readdirSync('images');
+  console.log(images);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(1337);
