e27d65c663a8c8449145e7ffaeaf8dc745e735f5
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-05-22 14:40:07 +0200

    Serve static files

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8888;
 
-http.createServer(function (req, res) {
-  var images = fs.readdirSync('images');
-  var output = '';
+http.createServer(function(request, response) {
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
 
-  for (var i in images) {
-    output += '<img src="/images/' + images[i] + '" /><br />';
+  if (uri == '/') {
+    var output = '';
+    var images = fs.readdirSync('images');
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+
+    for (var i in images) {
+      output += '<img src="/images/' + images[i] + '" /><br />';
+    }
+
+    response.end(output);
+
+    return;
   }
 
-  res.end(output);
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
 }).listen(1337);
