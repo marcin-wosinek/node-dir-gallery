a24489065eaf25e784dedd41a19c2b647e0caf89
marcin Soltysiuk <marcin.soltysiuk@contractors.roche.com>
2014-06-10 15:55:25 +0200

    display images

--- index.js
+++ index.js
@@ -6,9 +6,39 @@ var http = require("http"),
 
 http.createServer(function(request, response) {
   var uri = url.parse(request.url).pathname,
-    output = '', images, name,
-    href,
-    filename = path.join(process.cwd(), uri);
+    output = ['<html>'], name,
+    files, errorFiles,
+    href, linkId, resources,
+    domain = '',
+    fileName = path.join(process.cwd(), uri),
+    render = function(name, domain, uri) {
+      var output = [],
+        errors = 0,
+        href,
+        linkId,
+        errorFile = 'images/' + name + '/errors.txt';
+
+      if (name.indexOf('.png') > 0) {
+        linkId = name.match(/_(\d{1,5})\w*?\.png/)[1];
+        href = name.replace(/_\d{1,5}\w*?\.png/g, '').replace(/_/g, '/')
+          .replace(/\$1/g, '?').replace(/\$2/g, '&');
+        output = ['<li><h3 id="image-' + linkId + '">',
+          '<strong class="error" title="index in *urls.txt">' + linkId + '</strong> : ',
+          ' <a href="' + domain + href + '" class="title" target="blank">' + domain + href + '</a>',
+          ' <a href="#image-' + linkId + '"><em>#permlink</em></a></h3>',
+          '<img src="/images' + uri + "/" + name + '" /></li>'];
+      }
+      // folder
+      else if (fs.statSync('images' + uri + '/' + name).isDirectory()) {
+        href = uri + '/' + name;
+        if (fs.existsSync(errorFile)) {
+          errors = fs.readFileSync(errorFile);
+        }
+        output.push('<h3><a href="' + href + '">' + name +
+          '</a> <strong class="error" title="warnings, errors">' + errors + '</strong></h3>');
+      }
+      return output.join('');
+    };
 
   if (uri.indexOf('.png') < 0) {
     // to solve issue with slashes
@@ -16,38 +46,113 @@ http.createServer(function(request, response) {
       uri = '';
     }
 
+    if (uri.match('INT')) {
+      domain = 'http://synergy-int.roche.com';
+    }
+    else if (uri.match('INT5')) {
+      domain = 'http://synergy5-int.roche.com';
+    }
+    else if (uri.match('UAT')) {
+      domain = 'http://synergy-uat.roche.com';
+    }
+    else if (uri.match('UAT5')) {
+      domain = 'http://synergy5-uat.roche.com';
+    }
+    else if (uri.match('UAT5-EMAP')) {
+      domain = 'http://synergy5-emap.roche.com';
+    }
+    else if (uri.match('DEMO5')) {
+      domain = 'http://synergy5-demo.roche.com';
+    }
+    else if (uri.match('PROD')) {
+      domain = 'http://synergy.roche.com';
+    }
+    else if (uri.match('PROD5')) {
+      domain = 'http://synergy.intranet.roche.com';
+    }
+
     try {
-      images = fs.readdirSync('images' + uri);
+      files = fs.readdirSync('images' + uri);
     }
     catch (e) {
       response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Dir doen't exists\n");
+      response.write("404 Dir doesn't exists\n");
       response.end();
       return;
     }
 
+    resources = [
+      '<script src="http://minifiedjs.com/download/minified-web.js"></script>',
+      '<script>var _ = require("minified");',
+      'function ready() {',
+      '  _.$("#filter").on("keyup",function() {',
+      '    var value = this[0].value;',
+      '    _.$("ul li .title").each(function(node) {',
+      '      if (node.innerHTML.indexOf(value) != -1) {',
+      '       node.parentNode.parentNode.style.display = "block";',
+      '      } else {',
+      '       node.parentNode.parentNode.style.display = "none"; }',
+      '    })',
+      '  });',
+      '}',
+      '_.$.ready(ready);',
+      '</script>',
+      '<style>',
+      'h1 {background: #eee; padding: 10px;}',
+      'ul{padding:0}',
+      'ul li {border-bottom: solid 1px #f8f8f8;}',
+      '.errors a { color: c00;}',
+      'li em {background: #eee; border-radius: 6px; padding: 3px; margin-left: 15px;font-size: .7em}',
+      '.error {background: #ddd; border-radius: 6px; padding: 3px; margin-left: 15px;font-size: .7em}',
+      '.filter-container {position: fixed; top: 0; left: 0; width: 100%; background: #000; padding: 5px;}',
+      '</style>'
+    ];
+
     response.writeHead(200, {'Content-Type': 'text/html'});
+    output.push('<head>' + resources.join('') + '</head>');
 
-    for (var i in images) {
-      name = images[i];
-      href = name.replace(/_/g, '/').replace(/\$\{1\}/g, '?').replace(/_\d*\.png/g, '');
+    // render images
+    if (domain !== '') {
+      output.push('<div class="filter-container"><input id="filter" placeholder="filter images"/></div><br>');
 
-      // render image
-      if (name.indexOf('.png') > 0) {
-        output += '<h1><a href="' + href + '" target="blank">' + name + '</a></h1>';
-        output += '<img src="/images' + uri + "/" + name + '" /><br />';
+      errorFiles = files.filter(function(element) {
+        return element.indexOf('error.png') !== -1;
+      });
+      files = files.filter(function(element) {
+        return element.indexOf('error.png') === -1;
+      });
+
+      if (errorFiles.length) {
+        output.push('<h1>PAGES WITH ERRORS:</h3><ul class="errors">');
+        for (var i in errorFiles) {
+          output.push(render(errorFiles[i], domain, uri));
+        }
+        output.push('</ul>');
       }
       else {
-        output += '<h1><a href="' + uri + '/' + name + '">' + name + '</a></h1>';
+        output.push('<h1>NO ERRORS:</h1>');
       }
-    }
 
-    response.end(output);
+      output.push('<h1>PAGES WITHOUT ERRORS:</h3><ul>');
+      for (var i in files) {
+        output.push(render(files[i], domain, uri));
+      }
+
+      output.push('</ul></body></html>');
+    }
+    else {
+      output.push('<h1>ENVIRONMENTS:</h1>');
+      for (var i in files) {
+        output.push(render(files[i], domain, uri));
+      }
+      output.push('</body></html>');
+    }
+    response.end(output.join(''));
 
     return;
   }
 
-  fs.exists(filename, function(exists) {
+  fs.exists(fileName, function(exists) {
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
       response.write("404 Not Found\n");
@@ -55,9 +160,11 @@ http.createServer(function(request, response) {
       return;
     }
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+    if (fs.statSync(fileName).isDirectory()) {
+      fileName += '/index.html';
+    }
 
-    fs.readFile(filename, "binary", function(err, file) {
+    fs.readFile(fileName, "binary", function(err, file) {
       if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
