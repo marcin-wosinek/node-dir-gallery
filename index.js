4ff646a4eb0aa51cbdcc52d52211f7b5409249db
Marcin Wosinek <marcin.wosinek@gmail.com>
2014-06-11 14:28:05 +0200

    move 'remove' route to express

--- index.js
+++ index.js
@@ -12,87 +12,46 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/images', serveIndex(__dirname + '/images'));
 app.use('/images', express.static(__dirname + '/images'));
 
-app.use(function(request, response) {
-  var uri = url.parse(request.url).pathname,
-    output = ['<html>'], name,
-    files, errorFiles,
-    href, linkId,
-    domain = '',
-    fileName = path.join(process.cwd(), uri),
-    render = function(name, domain, uri) {
-      var output = [],
-        prefix = '',
-        errors = 0,
-        href,
-        linkId,
-        errorFile = 'images/' + name + '/errors.txt';
+var resources = [
+  '<meta name="viewport" content="width=device-width, initial-scale=1">',
+  '<script src="/public/script.js"></script>',
+  '<script src="http://minifiedjs.com/download/minified-web.js"></script>',
+  '<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">',
+  '<link rel="stylesheet" href="/public/site.css">'
+];
 
-      // image
-      if (name.match(/(.png|.jpg)/)) {
-        linkId = name.match(/_(\d{1,5})\w*?\.png/)[1];
-        prefix = name.match(/_\d{1,5}(\w*?)\.png/)[1];
-        href = name.replace(/_\d{1,5}\w*?\.png/g, '').replace(/_/g, '/')
-          .replace(/\$1/g, '?').replace(/\$2/g, '&');
-        output = [
-          '<li><h3 id="image-' + linkId + '">',
-          '<strong title="index in *urls.txt">' + linkId + '</strong> : ',
-          ' <a href="' + domain + href + '" class="title pure-button pure-button-primary" target="blank">',
-          domain + href + (prefix ? '#' + prefix : '') + '</a>',
-          ' <a href="#image-' + linkId + '"><em class="pure-button button-small">#permlink</em></a></h3>',
-          '<img src="/images' + uri + "/" + name + '" class="pure-img" /></li>'
-        ];
-      }
-      // folder
-      else if (fs.statSync('images' + uri + '/' + name).isDirectory()) {
-        href = uri + '/' + name;
-        if (fs.existsSync(errorFile)) {
-          errors = fs.readFileSync(errorFile);
-        }
-        output.push([
-          '<div class="pure-g ' + (errors ? 'warning' : 'success')+'">',
-          '<h3 class="pure-u-1-5"><a href="' + href + '">' + name + '</a></h3>',
-          '<div class="pure-u-4-5"><strong class="badge">' + errors + ' warnings, errors</strong>',
-          //' <a href="' + href + '/remove/ask" class="remove pure-button button-small pure-button-primary">remove folder</a>',
-          '</div></div><hr/>'
-        ].join(''));
-      }
-      return output.join('');
-    },
-    filter = '<div class="filter-container"><label>Filter <input id="filter"/><em></em></label></div>',
-    resources = [
-      '<meta name="viewport" content="width=device-width, initial-scale=1">',
-      '<script src="/public/script.js"></script>',
-      '<script src="http://minifiedjs.com/download/minified-web.js"></script>',
-      '<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">',
-      '<link rel="stylesheet" href="/public/site.css">'
-    ],
-    folders = [
-      '<div class="filter-container">',
-      '<strong>ENVIRONMENTS:</strong></div>'
-    ];
+// confirm remove folder
+app.use('/remove/ask', function(request, response) {
+  var uri = url.parse(request.url).pathname
+    , output = []
+    , href = uri.replace('/remove/ask', '');
 
-  // confirm remove folder
-  if (uri.match('/remove/ask')) {
-    href = uri.replace('/remove/ask', '');
-
-    if (fs.existsSync('images' + href)) {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      output.push('<head>' + resources.join('') + '</head>');
-      output.push([
-        '<h2>Remove folder "' + href + '"?</h2>',
-        ' <a href="' + href + '/remove" class="remove pure-button pure-button-primary">yes remove</a>',
-        ' <a href="' + href + '" class="remove pure-button">no</a>'
-      ].join(''));
-      response.end(output.join(''));
-    }
+  if (fs.existsSync('images' + href)) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    output.push('<head>' + resources.join('') + '</head>');
+    output.push([
+      '<h2>Remove folder "' + href + '"?</h2>',
+      ' <a href="/remove' + href + '" class="remove pure-button pure-button-primary">yes remove</a>',
+      ' <a href="' + href + '" class="remove pure-button">no</a>'
+    ].join(''));
+    response.end(output.join(''));
   }
-  // remove folder
-  else if (uri.match('/remove')) {
-    href = ('images' + uri).replace('/remove', '');
+});
 
-    if (fs.statSync(href).isDirectory()) {
-      fs.readdirSync(href).forEach(function(file, index) {
-        var curPath = href + '/' + file;
+// remove folder
+app.use('/remove', function(request, response) {
+  var uri = url.parse(request.url).pathname
+    , output = [];
+
+  var path = './images' + uri;
+
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  output.push('<head>' + resources.join('') + '</head>');
+
+  try {
+    if (fs.statSync(path).isDirectory()) {
+      fs.readdirSync(path).forEach(function(file, index) {
+        var curPath = path + '/' + file;
         if (fs.lstatSync(curPath).isDirectory()) { // recurse
           deleteFolderRecursive(curPath);
         }
@@ -100,15 +59,74 @@ app.use(function(request, response) {
           fs.unlinkSync(curPath);
         }
       });
-      fs.rmdirSync(href);
-
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      output.push('<head>' + resources.join('') + '</head>');
-      output.push('<h2>Folder removed: <em>' + href + '</em></h2>');
-      response.end(output.join(''));
+      fs.rmdirSync(path);
     }
+
+    output.push('<h2>Folder removed: <em>' + path + '</em></h2>');
+    output.push('<a href="/">Return</a>');
   }
-  else if (!uri.match('.png')) {
+  catch (e) {
+    output.push('<h2>Cannot remove folder: <em>' + path + '</em></h2>');
+    output.push('<pre>' + e + '</pre>');
+    output.push('<a href="/">Return</a>');
+  }
+
+  response.end(output.join(''));
+});
+
+app.use(function(request, response) {
+var uri = url.parse(request.url).pathname,
+  output = ['<html>'], name,
+  files, errorFiles,
+  href, linkId,
+  domain = '',
+  fileName = path.join(process.cwd(), uri),
+  render = function(name, domain, uri) {
+    var output = [],
+      prefix = '',
+      errors = 0,
+      href,
+      linkId,
+      errorFile = 'images/' + name + '/errors.txt';
+
+    // image
+    if (name.match(/(.png|.jpg)/)) {
+      linkId = name.match(/_(\d{1,5})\w*?\.png/)[1];
+      prefix = name.match(/_\d{1,5}(\w*?)\.png/)[1];
+      href = name.replace(/_\d{1,5}\w*?\.png/g, '').replace(/_/g, '/')
+        .replace(/\$1/g, '?').replace(/\$2/g, '&');
+      output = [
+        '<li><h3 id="image-' + linkId + '">',
+        '<strong title="index in *urls.txt">' + linkId + '</strong> : ',
+        ' <a href="' + domain + href + '" class="title pure-button pure-button-primary" target="blank">',
+        domain + href + (prefix ? '#' + prefix : '') + '</a>',
+        ' <a href="#image-' + linkId + '"><em class="pure-button button-small">#permlink</em></a></h3>',
+        '<img src="/images' + uri + "/" + name + '" class="pure-img" /></li>'
+      ];
+    }
+    // folder
+    else if (fs.statSync('images' + uri + '/' + name).isDirectory()) {
+      href = uri + '/' + name;
+      if (fs.existsSync(errorFile)) {
+        errors = fs.readFileSync(errorFile);
+      }
+      output.push([
+        '<div class="pure-g ' + (errors ? 'warning' : 'success')+'">',
+          '<h3 class="pure-u-1-5"><a href="' + href + '">' + name + '</a></h3>',
+          '<div class="pure-u-4-5"><strong class="badge">' + errors + ' warnings, errors</strong> ',
+          '<a href="/remove/ask' + href + '" class="remove pure-button button-small pure-button-primary">remove folder</a>',
+          '</div></div><hr/>'
+        ].join(''));
+      }
+      return output.join('');
+    },
+    filter = '<div class="filter-container"><label>Filter <input id="filter"/><em></em></label></div>',
+    folders = [
+      '<div class="filter-container">',
+      '<strong>ENVIRONMENTS:</strong></div>'
+    ];
+
+  if (!uri.match('.png')) {
     // to solve issue with slashes
     if (uri.length === 1) {
       uri = '';
